c359426f0676c7e40bdd875e441288016c4e2b47
Omar <98291870+OmarAbdelsalam-Tech@users.noreply.github.com>
2023-11-26 20:38:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,35 +3,14 @@ import numpy as np
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import load_model
 from PIL import Image
-import io
 import requests
 import os
 
 # Function to download the model file from Google Drive
-def download_model(file_id, destination):
-    URL = f"https://drive.google.com/uc?id={file_id}"
-    session = requests.Session()
-    response = session.get(URL, stream=True)
-    token = get_confirm_token(response)
-
-    if token:
-        params = {'confirm': token}
-        response = session.get(URL, params=params, stream=True)
-
-    save_response_content(response, destination)
-
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-    return None
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk:  # filter out keep-alive new chunks
-                f.write(chunk)
+def download_model(url, destination):
+    response = requests.get(url)
+    with open(destination, 'wb') as f:
+        f.write(response.content)
 
 # Function to preprocess the image
 def preprocess_image(uploaded_file):
@@ -49,23 +28,20 @@ st.write("Upload an image to classify if it's a motherboard or not.")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Show uploaded image
     st.image(uploaded_file, caption='Uploaded Image', use_column_width=True)
     
     if st.button('Classify'):
-        # Display a progress bar
         with st.spinner('Loading model and classifying...'):
-            # Download the model
             model_path = 'my_model.h5'
-            model_file_id = '1BLWDHXoA_mwbVKiMIAGcUapdCo47UwNU'  # Your Google Drive file ID
+            model_url = 'https://drive.google.com/uc?id=1BLWDHXoA_mwbVKiMIAGcUapdCo47UwNU'  # Your Google Drive file URL
             if not os.path.isfile(model_path):
-                download_model(model_file_id, model_path)
+                download_model(model_url, model_path)
 
-            # Load the model
-            model = load_model(model_path)
-
-            # Preprocess the image and make a prediction
-            image_data = preprocess_image(uploaded_file)
-            prediction = model.predict(image_data)
-            class_label = "Motherboard" if prediction[0][0] > 0.5 else "Not a Motherboard"
-            st.success(f"Prediction: {class_label}")
+            try:
+                model = load_model(model_path)
+                image_data = preprocess_image(uploaded_file)
+                prediction = model.predict(image_data)
+                class_label = "Motherboard" if prediction[0][0] > 0.5 else "Not a Motherboard"
+                st.success(f"Prediction: {class_label}")
+            except Exception as e:
+                st.error(f"Error loading model: {e}")
