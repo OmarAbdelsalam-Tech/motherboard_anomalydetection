7a328fdaa312e037a46c770b0906cd8ea18be545
Omar <98291870+OmarAbdelsalam-Tech@users.noreply.github.com>
2023-11-26 20:07:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from PIL import Image
 import io
 import requests
 import os
+import time
 
 # Function to download the model file from Google Drive
 def download_model(file_id, destination):
@@ -33,15 +34,6 @@ def save_response_content(response, destination):
             if chunk:  # filter out keep-alive new chunks
                 f.write(chunk)
 
-# Download the model
-model_path = 'my_model.h5'
-model_file_id = '1BLWDHXoA_mwbVKiMIAGcUapdCo47UwNU'  # Replace with your Google Drive file ID
-if not os.path.isfile(model_path):
-    download_model(model_file_id, model_path)
-
-# Load the model
-model = load_model(model_path)
-
 # Function to preprocess the image
 def preprocess_image(uploaded_file):
     img = Image.open(uploaded_file)
@@ -51,16 +43,30 @@ def preprocess_image(uploaded_file):
     img_array /= 255.0
     return img_array
 
-# Streamlit UI
+# Streamlit UI setup
 st.title("Motherboard Classification")
 st.write("Upload an image to classify if it's a motherboard or not.")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
 if uploaded_file is not None:
-    image_data = preprocess_image(uploaded_file)
-    st.image(image_data, caption='Uploaded Image', use_column_width=True)
+    # Show uploaded image
+    st.image(uploaded_file, caption='Uploaded Image', use_column_width=True)
     
     if st.button('Classify'):
-        prediction = model.predict(image_data)
-        class_label = "Motherboard" if prediction[0][0] > 0.5 else "Not a Motherboard"
-        st.write(f"Prediction: {class_label}")
+        # Display a progress bar
+        with st.spinner('Loading model and classifying...'):
+            # Download the model
+            model_path = 'my_model.h5'
+            model_file_id = '1BLWDHXoA_mwbVKiMIAGcUapdCo47UwNU'  # Replace with your Google Drive file ID
+            if not os.path.isfile(model_path):
+                download_model(model_file_id, model_path)
+
+            # Load the model
+            model = load_model(model_path)
+
+            # Preprocess the image and make a prediction
+            image_data = preprocess_image(uploaded_file)
+            prediction = model.predict(image_data)
+            class_label = "Motherboard" if prediction[0][0] > 0.5 else "Not a Motherboard"
+            st.success(f"Prediction: {class_label}")
